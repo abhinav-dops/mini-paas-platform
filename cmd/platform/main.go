09e438b40bc1d72db738d0b2b7fea8ff0620e811
abhinav-dops <abhinavsingh.ag@gmail.com>
2026-01-06 19:55:44 +0530

    http route created and tested

--- cmd/platform/main.go
+++ cmd/platform/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	log.Println("Mini PaaS Platform starting...")
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	log.Fatal(http.ListenAndServe(":9000", nil))
+}
