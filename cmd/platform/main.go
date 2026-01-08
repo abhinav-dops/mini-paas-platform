009a015ff881a3ee94ffad3e44f66e05a7d681cd
abhinav-dops <abhinavsingh.ag@gmail.com>
2026-01-08 14:50:16 +0530

    wire Api handlers into main server

--- cmd/platform/main.go
+++ cmd/platform/main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/abhinav-dops/mini-paas-platform/internal/api"
 )
 
 func main() {
 	log.Println("Mini PaaS Platform starting...")
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, _ *http.Request) {
-		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
+	http.HandleFunc("/apps/deploy", api.DeployApp)
+	http.HandleFunc("/apps", api.ListApps)
 
 	log.Fatal(http.ListenAndServe(":9000", nil))
 }
