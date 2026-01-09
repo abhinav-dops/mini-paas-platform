1b00e2a4ed752b59a97e5092d1aae4a5352b0acc
abhinav-dops <abhinavsingh.ag@gmail.com>
2026-01-10 00:06:13 +0530

    More Endpoints added

--- cmd/platform/main.go
+++ cmd/platform/main.go
@@ -13,8 +13,12 @@ func main() {
 	http.HandleFunc("/health", func(w http.ResponseWriter, _ *http.Request) {
 		w.Write([]byte("OK"))
 	})
+
 	http.HandleFunc("/apps/deploy", api.DeployApp)
 	http.HandleFunc("/apps", api.ListApps)
+	http.HandleFunc("/infra/provision", api.ProvisionInfra)
+	http.HandleFunc("/infra/status", api.GetInfraStatus)
+	http.HandleFunc("/infra/destroy", api.DestroyInfra)
 
 	log.Fatal(http.ListenAndServe(":9000", nil))
 }
